87cf77201d84a4c6db115b9cffc6d849f255bc5c
Jose Murinello <josem@telnyx.com>
2022-01-25 01:39:59 +0900

    refactor: remove unused imports

--- contexts/TelnyxMeetContext.ts
+++ contexts/TelnyxMeetContext.ts
@@ -1,5 +1,4 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import toast, { Toast } from 'react-hot-toast';
 
 const TelnyxMeetContext = React.createContext<{
   audioInputDeviceId: string | undefined;
