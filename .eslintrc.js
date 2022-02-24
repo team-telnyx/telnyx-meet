ba76b22eccff9d0a4a9cad83375033b0a513d7b3
Jose Murinello <josem@telnyx.com>
2022-02-24 17:32:10 +0900

    fix: eslint fail on build

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@next/next/recommended',
-    "next"
+    'next'
   ],
   globals: {
     Atomics: 'readonly',
