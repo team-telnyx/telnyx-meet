df2ff71d7dbac30b45c0ad5290166421ded0c448
Jose Murinello <josem@telnyx.com>
2022-03-23 18:01:33 +0900

    feat: include simulcast stream controls on screen share

--- components/Feeds.tsx
+++ components/Feeds.tsx
@@ -37,6 +37,7 @@ function Feeds({
         dominantSpeakerId={dominantSpeakerId}
         getParticipantStream={getParticipantStream}
         getStatsForParticipantStream={getStatsForParticipantStream}
+        updateSubscription={updateSubscription}
       />
     );
   }
--- components/ScreenSharingLayout.tsx
+++ components/ScreenSharingLayout.tsx
@@ -39,6 +39,7 @@ function ScreenSharingLayout({
   dominantSpeakerId,
   getParticipantStream,
   getStatsForParticipantStream,
+  updateSubscription,
   dataTestId,
 }: {
   participants: TelnyxRoom['state']['participants'];
@@ -48,6 +49,7 @@ function ScreenSharingLayout({
   dominantSpeakerId?: Participant['id'];
   getParticipantStream: TelnyxRoom['getParticipantStream'];
   getStatsForParticipantStream: TelnyxRoom['getWebRTCStatsForStream'];
+  updateSubscription: TelnyxRoom['updateSubscription'],
   dataTestId: string;
 }) {
   const USERS_PER_PAGE = 3;
@@ -93,6 +95,7 @@ function ScreenSharingLayout({
           isSpeaking={dominantSpeakerId === participant.id}
           mirrorVideo={participant.origin === 'local'}
           getStatsForParticipantStream={getStatsForParticipantStream}
+          updateSubscription={updateSubscription}
         />
       );
     })
@@ -129,6 +132,7 @@ function ScreenSharingLayout({
           stream={getParticipantStream(presenter.id, 'presentation')}
           isSpeaking={false}
           getStatsForParticipantStream={getStatsForParticipantStream}
+          updateSubscription={updateSubscription}
           mirrorVideo={false}
         />
       </div>
