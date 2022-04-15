bd17cc40053f96f67039d1a2f10c396d0c5b79d1
Jose Murinello <josem@telnyx.com>
2022-04-15 17:11:16 +0900

    feat: include experimental features on the fly

--- pages/rooms/[roomId].tsx
+++ pages/rooms/[roomId].tsx
@@ -5,23 +5,32 @@ import Rooms from './';
 
 const RoomId = () => {
   const router = useRouter();
-  const { roomId, network_metrics, client_token, refresh_token } =
-    router.query as {
-      roomId: string;
-      network_metrics: string;
-      client_token: string;
-      refresh_token: string;
-    };
+  const {
+    roomId,
+    client_token,
+    refresh_token,
+    network_metrics,
+  } = router.query as {
+    roomId: string;
+    client_token: string;
+    refresh_token: string;
+    network_metrics: string;
+  };
 
-  const showMetricsActionButton = network_metrics === 'true' ? true : false;
+  const isFeatureEnabled = (feature: string) =>
+    feature === 'true' ? true : false;
+
+  const enableExperimentalFeature = {
+    network_metrics: isFeatureEnabled(network_metrics),
+  };
 
   return (
     <Fragment>
       <Rooms
         id={roomId}
-        showMetricsActionButton={showMetricsActionButton}
         clientToken={client_token}
         refreshToken={refresh_token}
+        enableExperimentalFeature={enableExperimentalFeature}
       />
     </Fragment>
   );
