351b6b2ed31894ab240daa54917224e7482eca0a
Jose Murinello <16728775+muriware@users.noreply.github.com>
2022-06-08 20:58:54 +0900

    feat: return response message on server response status above 500 (#98)

--- pages/api/rooms/[roomId].ts
+++ pages/api/rooms/[roomId].ts
@@ -35,12 +35,8 @@ export default async function handler(
         notify(`${response.status}: Failed to get room`);
         transformFetchErrorToBugsnag(requestId, data, response.status);
 
-        if (response.status >= 500) {
-          res.status(response.status).end();
-        } else {
-          const json = await response.json();
-          res.status(response.status).json(json);
-        }
+        const json = await response.json();
+        res.status(response.status).json(json);
       }
     } catch (error) {
       notify(`request_id: ${requestId} - ${error}`);
