c0d79729969edabaf7d0126d42a3a19aaa3f0296
Deivid Veloso <veloso.deivid@gmail.com>
2022-07-08 11:10:41 -0300

    chore: create styles file for Feed

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
-import { Box, Text, Spinner, BoxExtendedProps } from 'grommet';
+import { Box, Text, Spinner } from 'grommet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faMicrophone,
@@ -13,77 +12,19 @@ import { TelnyxRoom } from 'hooks/room';
 
 import VideoTrack from 'components/VideoTrack';
 import { WebRTCStats } from 'components/WebRTCStats';
-import { NetworkMetricsMonitor } from './NetworkMetricsMonitor';
+import { NetworkMetricsMonitor } from 'components/NetworkMetricsMonitor';
 import { VideoBitrate } from 'components/VideoBitrate';
 import { VirtualBackground } from 'utils/virtualBackground';
-
-const VIDEO_BG_COLOR = '#111';
+import {
+  FeedContainer,
+  FeedFooter,
+  FeedHeader,
+  FeedParticipantNameCenter,
+  FeedSpinner,
+  VideoContainer,
+} from './styles';
 
 const allowedBrowsers = ['Chrome', 'Safari'];
-
-const FeedContainer = styled.div<{
-  isPresentation: boolean;
-  showAudioActivityIndicator: boolean;
-}>`
-  ${({ isPresentation, showAudioActivityIndicator }) => css`
-    position: relative;
-    overflow: hidden;
-    height: ${isPresentation ? '100%' : 'unset'};
-    padding-top: ${isPresentation
-      ? 'unset'
-      : `${(9 / 16) * 100}%`}; // 56.25% - 16:9 Aspect Ratio
-    background-color: ${VIDEO_BG_COLOR};
-    border-width: 3px;
-    border-style: solid;
-    border-color: ${showAudioActivityIndicator ? 'yellow' : '#1b1b1b'};
-  `}
-`;
-
-const FeedHeader = styled.div<{ showBlackBackgroundColor: boolean }>`
-  ${({ showBlackBackgroundColor }) => css`
-    position: absolute;
-    top: 0px;
-    z-index: 2;
-    width: 100%;
-    height: 100%;
-    background-color: ${showBlackBackgroundColor ? VIDEO_BG_COLOR : ''};
-  `}
-`;
-
-const VideoContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-`;
-
-const FeedFooter = styled.div`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  z-index: 2;
-`;
-
-const FeedParticipantNameCenter = styled(Box)<
-  BoxExtendedProps & {
-    showBlackBackgroundColor: boolean;
-  }
->`
-  ${({ showBlackBackgroundColor }) => css`
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: 2;
-    background-color: ${showBlackBackgroundColor ? VIDEO_BG_COLOR : ''};
-  `}
-`;
-
-const FeedSpinner = styled.div`
-  position: absolute;
-  top: 5px;
-  right: 5px;
-`;
 
 type FeedProps = {
   participant: Participant;
