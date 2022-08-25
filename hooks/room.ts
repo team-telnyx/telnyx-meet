6a07bac4ae654a924cfa28e24679ad377dbcde2d
Deivid Veloso <veloso.deivid@gmail.com>
2022-08-25 09:51:23 -0300

    chore: upgrade video sdk

--- hooks/room.ts
+++ hooks/room.ts
@@ -413,7 +413,7 @@ export const useRoom = ({
         });
       }
 
-      await roomRef.current.connect();
+      await roomRef.current.connect({ timeout: 1 * 1000 });
     };
 
     if (!roomRef.current) {
